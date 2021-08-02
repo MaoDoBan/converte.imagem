de24582048753445e2570bdf30fb8f48af1fda98
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-02 13:23:37 -0300

    Add buscaSequencial

--- src/classes/Node.ts
+++ src/classes/Node.ts
@@ -3,7 +3,8 @@ import { Color } from "./Color.ts";
 import { Neighbors } from "./Neighbors.ts";
 
 export class Node{
-  private static allNodes: { [hex: string]: Node } = {};
+  private static dictionaryAllNodes: { [hex: string]: Node } = {};
+  static readonly allNodes: Node[] = [];
   private neighbors: Neighbors;
 
   constructor(
@@ -11,7 +12,8 @@ export class Node{
     private color: Color
   ){
     this.neighbors = new Neighbors();
-    Node.allNodes[this.color.hex] = this;
+    Node.dictionaryAllNodes[this.color.hex] = this;
+    Node.allNodes.push(this);
   }
 
   get colorRgb(): number[]{
@@ -28,8 +30,18 @@ export class Node{
     return Color.calculateColorDistance(this.colorRgb, otherColorRgb);
   }
 
+  // static get arrayAllNodes(): Array<Node>{
+  //   return Object.values(Node.allNodes);
+  // }
+  // static get lengthAllNodes(): number{
+  //   return Object.keys(Node.allNodes).length;
+  // }
+  static getNode(hex: string): Node{
+    return Node.dictionaryAllNodes[hex];
+  }
+
   static async populateAllNodes(): Promise<boolean>{
-    if(Object.keys(Node.allNodes).length > 0) return false;
+    if(Node.allNodes.length > 0) return false;
 
     type RawPalette = { [blockId: number]: string[] };
     const rawPalette: RawPalette = await readJson("src/colorPalette/hexColorPalette.json") as RawPalette;
@@ -42,18 +54,32 @@ export class Node{
       }
     }
 
-    console.log("Populated "+Object.keys(Node.allNodes).length+" Nodes");
+    console.log("Populated "+Node.allNodes.length+" Nodes");
     return true;
   }
 
-  static growTree(limitNeighbors: number){
-    return 0;
+  // static growTree(limitNeighbors: number){
+  //   return 0;
+  // }
+
+  static buscaSequencial(targetColorRgb: number[]): Node{
+    const targetColorHex = Color.rgbToHex(targetColorRgb);
+    const targetNode = Node.dictionaryAllNodes[targetColorHex];
+    if(targetNode !== undefined) return targetNode;
+
+    let closerToTarget = {distance: 444, node: Node.allNodes[0]};
+    for(const node of Node.allNodes){
+      const distanceToNode = node.distanceToRgb(targetColorRgb);
+      if(distanceToNode < closerToTarget.distance){
+        closerToTarget = {distance: distanceToNode, node: node};
+      }
+    }
+
+    console.log("O resultado da busca sequencial estava próximo do alvo em "+closerToTarget.distance);
+    return closerToTarget.node;
   }
 
-  // static buscaSequencial(): Node{
-  //   ;
+  // buscaEmLargura(){}
+  // buscaInformada(){///partindo do centro; partindo de 8 cantos; partindo de 6 cantos; variações das duas anteriores + centro
   // }
-  buscaEmLargura(){}
-  buscaInformada(){///partindo do centro; partindo de 8 cantos; partindo de 6 cantos; variações das duas anteriores + centro
-  }
 }
