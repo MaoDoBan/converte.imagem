4b21ada9cce0430a1ed8fd5eb80f28ded9bb93bf
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-02 20:41:11 -0300

    Atualizações e começo do .growTree()

--- src/classes/Node.ts
+++ src/classes/Node.ts
@@ -1,17 +1,19 @@
 import { readJson } from "https://deno.land/x/jsonfile@1.0.0/mod.ts";
 import { Color } from "./Color.ts";
-import { Neighbors } from "./Neighbors.ts";
+import { Neighbor } from "./Neighbor.ts";
+import { NeighborsManager } from "./NeighborsManager.ts";
+import { ListNearNeighbors } from "./ListNearNeighbors.ts";
 
 export class Node{
   private static dictionaryAllNodes: { [hex: string]: Node } = {};
   static readonly allNodes: Node[] = [];
-  private neighbors: Neighbors;
+  private neighbors: NeighborsManager;
 
   constructor(
     readonly blockIdAndData: {blockId: number, metadata: number},
     private color: Color
   ){
-    this.neighbors = new Neighbors();
+    this.neighbors = new NeighborsManager( this.id );
     Node.dictionaryAllNodes[this.color.hex] = this;
     Node.allNodes.push(this);
   }
@@ -22,6 +24,9 @@ export class Node{
   get id(): string{
     return this.color.hex;
   }
+  static getNeighbors(nodeId: string): NeighborsManager{
+    return Node.dictionaryAllNodes[nodeId].neighbors;
+  }
 
   distanceToNode(otherNode: Node): number{
     return this.neighbors.distanceTo(otherNode.id) || Color.calculateColorDistance(this.colorRgb, otherNode.colorRgb);
@@ -30,12 +35,6 @@ export class Node{
     return Color.calculateColorDistance(this.colorRgb, otherColorRgb);
   }
 
-  // static get arrayAllNodes(): Array<Node>{
-  //   return Object.values(Node.allNodes);
-  // }
-  // static get lengthAllNodes(): number{
-  //   return Object.keys(Node.allNodes).length;
-  // }
   static getNode(hex: string): Node{
     return Node.dictionaryAllNodes[hex];
   }
@@ -48,8 +47,9 @@ export class Node{
 
     for(let blockId = 667; blockId <= 682; blockId++){
       for(let metadata = 0; metadata <= 15; metadata++){
-        const block = {blockId, metadata};
         const colorHex = rawPalette[blockId][metadata];
+        if(Node.dictionaryAllNodes[colorHex]) continue;//evita cor duplicada
+        const block = {blockId, metadata};
         new Node(block, new Color(colorHex));
       }
     }
@@ -58,19 +58,39 @@ export class Node{
     return true;
   }
 
-  // static growTree(limitNeighbors: number){
-  //   return 0;
-  // }
+  static growTree(limitNeighbors: number): boolean{///inacabada
+    for(const node of Node.allNodes){
+      const nearNeighbors = new ListNearNeighbors(limitNeighbors);
+      for(const otherNode of Node.allNodes){
+        if( node.id === otherNode.id ) continue;
 
-  static buscaSequencial(targetColorRgb: number[]): Node{
+        const distance = node.distanceToNode(otherNode);
+        nearNeighbors.add( new Neighbor(otherNode.id, distance) );
+
+
+        ///node.neighbors.distanceTo(otherNode.id)
+        //setar near
+
+        //se precisa, setar unique e salvar nos catalogados
+
+        // color.addDistance(otherColor.hex, distance);
+        // otherColor.addDistance(color.hex, distance);
+      }
+      node.neighbors.addSeveral(nearNeighbors);
+    }
+    return true;
+  }
+
+  static buscaSequencial(targetColorRgb: number[], mode: "strict" | "near" = "strict"): Node{
     const targetColorHex = Color.rgbToHex(targetColorRgb);
     const targetNode = Node.dictionaryAllNodes[targetColorHex];
-    if(targetNode !== undefined) return targetNode;
+    if(targetNode !== undefined && mode === "strict") return targetNode;
 
     let closerToTarget = {distance: 444, node: Node.allNodes[0]};
     for(const node of Node.allNodes){
       const distanceToNode = node.distanceToRgb(targetColorRgb);
       if(distanceToNode < closerToTarget.distance){
+        if(distanceToNode === 0 && mode === "near") continue;
         closerToTarget = {distance: distanceToNode, node: node};
       }
     }
@@ -78,8 +98,9 @@ export class Node{
     console.log("O resultado da busca sequencial estava próximo do alvo em "+closerToTarget.distance);
     return closerToTarget.node;
   }
-
   // buscaEmLargura(){}
+
   // buscaInformada(){///partindo do centro; partindo de 8 cantos; partindo de 6 cantos; variações das duas anteriores + centro
+  //   ;
   // }
 }
