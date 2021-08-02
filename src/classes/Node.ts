c642a8ef8795e72d70d4dcb1cf2414623a9df17e
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-02 11:23:10 -0300

    Boa parte da estrutura da 0.3

--- src/classes/Node.ts
+++ src/classes/Node.ts
@@ -1,15 +1,17 @@
+import { readJson } from "https://deno.land/x/jsonfile@1.0.0/mod.ts";
 import { Color } from "./Color.ts";
 import { Neighbors } from "./Neighbors.ts";
 
 export class Node{
-  private static allNodes: { [hex: string]: Node[] };
+  private static allNodes: { [hex: string]: Node } = {};
   private neighbors: Neighbors;
 
   constructor(
-    readonly blockIdAndData: {blockId: number, data: number},
+    readonly blockIdAndData: {blockId: number, metadata: number},
     private color: Color
   ){
     this.neighbors = new Neighbors();
+    Node.allNodes[this.color.hex] = this;
   }
 
   get colorRgb(): number[]{
@@ -19,10 +21,6 @@ export class Node{
     return this.color.hex;
   }
 
-  static growTree(limitNeighbors: number){
-    ;
-  }
-
   distanceToNode(otherNode: Node): number{
     return this.neighbors.distanceTo(otherNode.id) || Color.calculateColorDistance(this.colorRgb, otherNode.colorRgb);
   }
@@ -30,7 +28,31 @@ export class Node{
     return Color.calculateColorDistance(this.colorRgb, otherColorRgb);
   }
 
-  buscaSequencial(){}
+  static async populateAllNodes(): Promise<boolean>{
+    if(Object.keys(Node.allNodes).length > 0) return false;
+
+    type RawPalette = { [blockId: number]: string[] };
+    const rawPalette: RawPalette = await readJson("src/colorPalette/hexColorPalette.json") as RawPalette;
+
+    for(let blockId = 667; blockId <= 682; blockId++){
+      for(let metadata = 0; metadata <= 15; metadata++){
+        const block = {blockId, metadata};
+        const colorHex = rawPalette[blockId][metadata];
+        new Node(block, new Color(colorHex));
+      }
+    }
+
+    console.log("Populated "+Object.keys(Node.allNodes).length+" Nodes");
+    return true;
+  }
+
+  static growTree(limitNeighbors: number){
+    return 0;
+  }
+
+  // static buscaSequencial(): Node{
+  //   ;
+  // }
   buscaEmLargura(){}
   buscaInformada(){///partindo do centro; partindo de 8 cantos; partindo de 6 cantos; variações das duas anteriores + centro
   }
