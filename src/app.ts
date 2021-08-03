0d2bc49b025d69009ad272a4ef5d4ebc1c782c4e
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-03 19:10:40 -0300

    Tests and anotations to 0.4

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 import { Converter } from "./classes/Converter.ts";
 
 const converter = new Converter();
-converter.convert("t3x3");
+converter.convert("Logo_of_Stardew_Valley.png");//"t3x3" "maum"
--- src/proto.ts
+++ src/proto.ts
@@ -10,9 +10,9 @@ console.log(await Node.populateAllNodes());
 //console.log(Node.growTree(32));
 
 const past = Date.now();
-console.log("buscando 0 0 0: ",Node.sequentialSearch([0,0,0]));
-console.log("buscando perto 0 0 0: ",Node.sequentialSearch([0,0,0], "near"));
-console.log("buscando 128 128 128: ",Node.sequentialSearch([128,128,128]));
+// console.log("buscando 0 0 0: ",Node.sequentialSearch([0,0,0]));
+// console.log("buscando perto 0 0 0: ",Node.sequentialSearch([0,0,0], "near"));
+// console.log("buscando 128 128 128: ",Node.sequentialSearch([128,128,128]));
 
 
 
