1639799cf56b3e615648f1080d942cf4bce3c1f7
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-03 19:11:28 -0300

    Fix: test filename incorrect

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 import { Converter } from "./classes/Converter.ts";
 
 const converter = new Converter();
-converter.convert("Logo_of_Stardew_Valley.png");//"t3x3" "maum"
+converter.convert("Logo_of_Stardew_Valley");//"t3x3" "maum"
