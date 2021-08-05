ce4d7225436210a45b414f5012b0867ca955ebd1
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-04 23:14:23 -0300

    pixelLines direction Y

--- src/classes/Converter.ts
+++ src/classes/Converter.ts
@@ -19,47 +19,65 @@ export class Converter{
 
     const rawImage = await Deno.readFile("io/"+fileName+".png");
     this.image = await Image.decode(rawImage);
-    const blockMatrixString = this.imageToMatrix();
+    ///const blockMatrixStringX = this.imageToMatrix("x");
+    const blockMatrixStringY = this.imageToMatrix("y");
     //-console.log("Matriz:",blockMatrixString);
 
-    Converter.saveLua(fileName, 'local a="a"; Dados={'+orientation+','+blockMatrixString+'}');
+    Converter.saveLua(fileName, 'local a="a"; Dados={'+orientation+','+blockMatrixStringY+'}');
     console.log("Demorou ms: "+(Date.now()-past)+"\nO script vai gerar "+this.ct+" blocos"); this.ct = 0;
   }
   
-  private imageToMatrix(){
+  private imageToMatrix(direction: "x" | "y"){
     this.x = 0;
     this.y = this.image.height-1;
     //-this.previous = new PreviousInfos();
     let matrixString = "";
-    for(const line of this.pixelLines()){
+    for(const line of this.pixelLines(direction)){
       matrixString += this.pixelLineToString(line);
       this.y--;
       this.x = 0;
     }
     return matrixString.slice(0, -1);//removendo o último caractere
   }
-  private pixelLines(): Uint8ClampedArray[]{
+  private pixelLines(direction: "x" | "y"): Uint8ClampedArray[] | number[][]{
+    const bitmap = this.image.bitmap;
     const lines: Uint8ClampedArray[] = [];
-    for(let iRow = 0; iRow < this.image.bitmap.length; iRow += this.image.width*4){
-      lines.push( this.image.bitmap.subarray( iRow, iRow + this.image.width*4 ) );
+    if(direction == "x"){
+      for(let iRow = 0; iRow < bitmap.length; iRow += this.image.width*4){
+        lines.push( bitmap.subarray( iRow, iRow + this.image.width*4 ) );
+      }
+      return lines;
+    }
+    return this.pixelLineY();
+  }
+  private pixelLineY(): number[][]{
+    const bitmap = this.image.bitmap;
+    const lines: number[][] = [];
+    for(let iColumn = 0; iColumn < this.image.width; iColumn++){
+      const line: number[] = [];
+      for(let iLine = 0; iLine < this.image.height; iLine++){
+        const i = iColumn*4 + iLine*4*this.image.width;//cada pixel ocupa 4 índices
+        line.push(bitmap[i], bitmap[i+1], bitmap[i+2], bitmap[i+3]);
+      }
+      lines.push(line);
     }
     return lines;
   }
-  private pixelLineToString(line: Uint8ClampedArray): string{
+  private pixelLineToString(line: Uint8ClampedArray | number[]): string{
     //-console.log("line:\n",line.toString());
-    const segMananger = new SegmentManager();
+    const segManager = new SegmentManager();
     for(let iPixel = 0; iPixel < line.length; iPixel+=4){
       if(line[iPixel+3] < 128){//opacidade menor que 50%
         this.x++;
-        segMananger.closeCurrentSegment();
+        segManager.closeCurrentSegment();
         continue;
       }
 
       const block = this.pixelToBlock([ line[iPixel], line[iPixel+1], line[iPixel+2] ]);
-      segMananger.update(block, this.x, this.y);
+      segManager.update(block, this.x, this.y);
       this.x++; this.ct++;
     }
-    return segMananger.allToString();
+    return segManager.allToString();
   }
   private pixelToBlock(rgb: number[]): Block{
     const colorHex = Color.rgbToHex(rgb);
