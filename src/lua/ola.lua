de3c3c8cdf8e501d6a819f5553184af3aa7f622f
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-05 20:52:17 -0300

    Tests on lua

--- src/lua/ola.lua
+++ src/lua/ola.lua
@@ -1,6 +1,9 @@
 print("oi vc ai, me da um dinheiro ai")
 
-local bah = {1,2,3};
-local a,b,c = unpack(bah);
+-- local bah = {1,2,3};
+-- local a,b,c = unpack(bah);
 
-print(a,b,c);
+-- print(a,b,c);
+
+local a,b,c=3,2,1
+print(a,b,c)
