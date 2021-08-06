5067fb6a738b2b51a757c9fa99d8f11aec3ddda6
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-06 07:50:50 -0300

    Removing old deprecated classes

--- src/classes/Deprecated/Distances.ts
+++
@@ -1,9 +0,0 @@
-export class Distances{
-  [hex: string]: number;
-
-  get length(): number {
-    return Object.keys(this).length;
-  }
-}
-
-//export type Distance = {hex: string, distance: number};
