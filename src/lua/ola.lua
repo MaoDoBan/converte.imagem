8619d8232b10055f436b27895f2046832d6b7f68
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-06 15:27:13 -0300

    Tests on lua

--- src/lua/ola.lua
+++ src/lua/ola.lua
@@ -5,5 +5,19 @@ print("oi vc ai, me da um dinheiro ai")
 
 -- print(a,b,c);
 
-local a,b,c=3,2,1
-print(a,b,c)
+-- local a,b,c=3,2,1
+-- print(a,b,c)
+
+-- for i=0, 256 do
+--   print( i, ("").char(i+96) );
+-- end
+
+-- print(tonumber("7DE",16))
+-- local times = {
+--   ["time1"] = 12,
+--   ["time2"] = 15
+-- }
+
+local testet = {a=12,b=13,c=14}
+
+print(testet.a,testet["b"],testet.c)
