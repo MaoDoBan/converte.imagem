e316564e39c5747cd1b89b870a2a62a813e030b3
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-07 20:56:16 -0300

    Depurando com a image t3x3

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { ConversionManager } from "./classes/Convert/ConversionManager.ts";
 
 const conversor = new ConversionManager();
-await conversor.convert("arco-iris.png");//"t3x3" "maum"
+await conversor.convert("t3x3.png");// "maum.png"  "arco-iris.png"
 ///await conversor.saveLua("arco-iris");
