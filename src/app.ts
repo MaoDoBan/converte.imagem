33b380bf05c6a884908e009f4ec3a1724bb5015b
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-07 06:38:15 -0300

    Change import path to class Converter

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Converter } from "./classes/Converter.ts";
+import { Converter } from "./classes/Convert/Converter.ts";
 
 const converter = new Converter();
 await converter.convert("arco-iris.png");//"t3x3" "maum"
