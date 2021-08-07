82fb1ef79825dfcea5994cc561b52e97fb92ad49
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-07 09:00:35 -0300

    Initial anottaions

---
+++ src/classes/Convert/Encoder.ts
@@ -0,0 +1,56 @@
+import { Counter } from "./Counter.ts";
+type DictNumToString = { [num: number]: string };
+
+export class Encoder{
+  constructor()
+
+  toString(): string{
+    if(!this.segmentIsClosed) this.mergeSegment(andMergeBlock);
+  
+    const numToLetter = this.getNumberToLetter();
+  
+    ///fazer var na classe pra guardar o "local a,b,c=123,21,2"
+    let segmentsString = "";
+    for(const segment of this.segments){
+      this.segmentNumToLetter(segment, numToLetter);
+      segmentsString += '{'+segment.toString()+'},';
+    }
+    return segmentsString;
+  }
+
+  private getNumberToLetter(): DictNumToString{
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const limit = this.counter.length < letters.length ? this.counter.length : letters.length;
+    const repeatedDict: DictNumToString = {};
+  
+    for(let i = 0; i < limit; i++){
+      const highest = this.counter.popHighest();
+      repeatedDict[highest] = letters[i];
+    }
+    return repeatedDict;
+  }
+  private segmentNumToLetter(segment: NumOrString[], dict: DictNumToString){
+    for(let i = 0; i < segment.length; i++){
+      if( dict[ segment[i] as number ] ) segment[i] = dict[ segment[i] as number ];
+    }
+  }
+}
+
+
+/*toStringOld(): string{////delete on 0.5
+  let values = "";
+
+  for(let i = 0; i < limit; i++){
+    segmentsString += letters[i]+',';
+  }
+  if(limit > 0){
+    segmentsString = 
+      "local "+segmentsString.slice(0, -1)+//removendo o último caractere
+      "="+values.slice(0, -1)+"\n";
+  }
+}*/
+
+  /*private imageToStringMatrix(){
+    const blockMatrix = this.imageToBlockMatrix();
+    return matrixString.slice(0, -1);//removendo o último caractere
+  }*/
