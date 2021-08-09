3d002b20617194743bfd715b6df19c8216558d98
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-09 09:15:19 -0300

    feat Counter: base structure
    - constructor, initializeCountedBlocks(), countBlocks()

--- src/classes/Convert/Counter.ts
+++ src/classes/Convert/Counter.ts
@@ -1,46 +1,55 @@
+import { NumOrString } from "../../interfaces/Types.ts";
+type CountMap = { [block: string]: number };
+
 export class Counter{ ////need rework
-  private counter: number[];
+  private countedBlocks: CountMap;
 
   constructor(
-    private limit = 682
+    private blockImages: NumOrString[][]
   ){
-    const lowerLimit = 272;
-    this.counter = [];
-    for(let i = 10; i <= lowerLimit; i++){
-      this.counter[i] = 0;
-    }
-    for(let i = 667; i <= limit; i++){
-      this.counter[i] = 0;
-    }
+    this.countedBlocks = this.initializeCountedBlocks();
   }
 
-  get length(): number{
-    let ct = 0;
-    for(let i = 667; i <= this.limit; i++){
-      if(this.counter[i]) ct++;
-    }
-    return ct;
-  }
-
-  increment(...numbers: number[]){
-    for(const num of numbers){
-      if(num < 10 || num > this.limit){
-        continue;
-      }
-      this.counter[num] += 2;
-      if(num >= 100) this.counter[num]++;//se 100+ vai ocupar 3 caracteres
-    }
-  }
-
-  popHighestToHex(): string{////
-    let max = {num: -1, count: 0};
-    for(let i = 10; i < this.counter.length; i++){
-      if(this.counter[i] && this.counter[i] > max.count){
-        max.num = i;
-        max.count = this.counter[i];
+  countBlocks(){
+    for(const blockImage of this.blockImages){
+      for(let i = 6; i < blockImage.length; i++){
+        const block = blockImage[i] as string;
+        this.countedBlocks[ block ] += block.length;
       }
     }
-    delete this.counter[max.num];
-    return max.num;
+    console.log("Blocos contados:",this.countedBlocks);
+  }
+
+  private initializeCountedBlocks(): CountMap{
+    const initialCountedBlocks: CountMap = {};
+    for(const blockImage of this.blockImages){
+      for(let i = 6; i < blockImage.length; i++){
+        initialCountedBlocks[ blockImage[i] ] = 0;
+      }
+    }
+    return initialCountedBlocks;
   }
 }
+
+
+/*increment(...numbers: number[]){
+  for(const num of numbers){
+    if(num < 10 || num > this.limit){
+      continue;
+    }
+    this.counter[num] += 2;
+    if(num >= 100) this.counter[num]++;//se 100+ vai ocupar 3 caracteres
+  }
+}*/
+
+/*popHighestToHex(): string{////
+  let max = {num: -1, count: 0};
+  for(let i = 10; i < this.counter.length; i++){
+    if(this.counter[i] && this.counter[i] > max.count){
+      max.num = i;
+      max.count = this.counter[i];
+    }
+  }
+  delete this.counter[max.num];
+  return max.num;
+}*/
