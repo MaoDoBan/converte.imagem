f73dff081bb3c9604efc22ee93666cdef2c6fdf1
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-09 09:16:15 -0300

    chore: studing and testing some lua things

--- src/lua/ola.lua
+++ src/lua/ola.lua
@@ -18,6 +18,41 @@ print("oi vc ai, me da um dinheiro ai")
 --   ["time2"] = 15
 -- }
 
-local testet = {a=12,b=13,c=14}
+-- local testet = {a=12,b=13,c=14}
 
-print(testet.a,testet["b"],testet.c)
+-- print(testet.a,testet["b"],testet.c)
+
+
+-- local func_event = function (params)
+--   threadpool:wait(1)
+--   Chat:sendChat("teste1")
+--   threadpool:wait(3)
+--   Chat:sendChat("teste2")
+-- end
+-- ScriptSupportEvent:registerEvent('Player.PlayAction', func_event)
+
+local palavras = {"amor", "flor", "estrela", "árvores", "abacaxi"};
+-- for i=1, 20 do
+--   local rnum = math.random(#palavras);
+--   print(palavras[rnum]);
+--   table.remove(palavras, rnum);
+--   if(#palavras == nil) then break end
+-- end
+-- repeat
+--   local t = #palavras;
+--   print(#palavras);
+--   palavras[#palavras] = nil;
+--   io.read("*l")
+-- until t == 0
+
+-- print(#palavras);
+-- palavras[#palavras] = nil
+-- print(#palavras);
+-- local qtp = #palavras
+-- print(qtp)
+
+function clonar(org)
+  return {table.unpack(org)}
+end
+local p2 = clonar(palavras)
+print(#p2, #palavras)
