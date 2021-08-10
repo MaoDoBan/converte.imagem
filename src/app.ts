ba01dcb08b4cb99270f7af2aa6b62d99e982ecac
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-10 16:50:39 -0300

    feat app: suport command line fileName

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { ConversionManager } from "./classes/Convert/ConversionManager.ts";
 
 const conversor = new ConversionManager();
-await conversor.convert({fileName: "maum.png"});//"t3x3.png" "maum.png"  "arco-iris.png"
-await conversor.save("encoder-teste");
+await conversor.convert({fileName: Deno.args[0]});//"t3x3.png" "maum.png"  "arco-iris.png"
+await conversor.save(Deno.args[0].split('.')[0] + ".lua");
