21afc343ba4d2f67b1211a87543c2547c3b0ef85
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-10 12:13:04 -0300

    refactor Encoder: +blocksDict -asssemble() deprecated +encode() mount blocksDict and assemble .encoded

--- src/classes/Convert/Encoder.ts
+++ src/classes/Convert/Encoder.ts
@@ -1,9 +1,10 @@
 import { NumOrString } from "../../interfaces/Types.ts";
 import { Counter } from "./Counter.ts";
-//type DictNumToString = { [num: number]: string };
+type BlocksDict = { [block: string]: string };
 
 export class Encoder{
   private counter: Counter;
+  private blocksDict: BlocksDict;///uma classe só pra isso? com .toString()
   private dict: string;
   private encoded: string;
 
@@ -11,23 +12,25 @@ export class Encoder{
     private blockImages: NumOrString[][]
   ){
     this.counter = new Counter(this.blockImages);
+    this.blocksDict = {};
     this.dict = "";
     this.encoded = "";
   }
 
   get result(): string{
-    this.arrayToString();
+    this.run();
     return ""////this.dict + this.encoded;
   }
 
-  private arrayToString(){
+  private run(){
     const qtUniques = this.counter.countBlocks();
+    this.encode();
 
     ///se arrayContagem.length maior q 20'000: retorna console.log erro limite
     if(qtUniques > 20000) return console.log("ERRO: contagem de dict excedeu 20k");
 
     ///se arrayContagem.length menor que 52: retorna montando dict com A-Z
-    if(qtUniques <= 52) return this.assemble(qtUniques);
+    if(qtUniques <= 52) return //this.assemble(qtUniques);
 
     ///monta os 46 mais repetidos em a-z A-T, remove eles do array de contagem
 
@@ -35,7 +38,37 @@ export class Encoder{
 
     // 
   }
-  private assemble(limit: number){///, keyLength: number
+
+  private encode(){
+    //const specialChars = " .,;:-=+";
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";//.split('');
+    for(let i = 0; i < this.counter.length; i++){
+      this.blocksDict[ this.counter.popHighest() ] = letters[i];
+    }
+
+    let lastBlockIsOnDict = false;
+    let dictEquivalent: string | undefined;
+    let block: string;
+    for(const blockImage of this.blockImages){
+      for(let i = 6; i < blockImage.length; i++){
+        block = blockImage[i] as string;
+        dictEquivalent = this.blocksDict[block];
+        if(dictEquivalent){
+          if(lastBlockIsOnDict){
+            this.encoded += '.';//specialChars[block.length];
+            lastBlockIsOnDict = false;
+          }
+          this.encoded += dictEquivalent;
+          continue;
+        }
+        this.encoded += ","+block;
+        lastBlockIsOnDict = true;
+      }
+    }
+    console.log("output:",this.blocksDict,this.encoded,this.encoded.length);
+  }
+
+  /*private assemble(limit: number){///, keyLength: number
     const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let highest: string;
     const removedBlocks = this.counter.popUnnecessaryWith(1);
@@ -50,10 +83,6 @@ export class Encoder{
       }
     }
     console.log("output: ", this.dict, this.encoded);
-  }
-
-  /*private encodeString(){
-    ;
   }*/
 }
     ///-------se array de contagem vazio: retorna
