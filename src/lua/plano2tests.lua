9d91cfdd40abed7b14c210647178de86f2c57da2
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-11 05:26:25 -0300

    feat: new plano2tests.lua +Dict pra teste +Dados pra teste +Simbolos pra ler Dados +clique() reconhece só clique de cajado, manda gerar imagem +geraImagem(): ++cria objeto de controle, ++loop nos dadosCompactos pra avaliar cada caractere +avaliaCaractere(): ++lê trechos base36 de Dados, ++lê do Dict pra base36, ++manda os itens base36 pro converteEGera +classes vazias pra implementar depois

---
+++ src/lua/plano2tests.lua
@@ -0,0 +1,72 @@
+Dict={a="tf",b="eg",c="f7",d="ho",e="17n",f="j6",g="i4",h="2eb",i="wc",j="so",k="1ak",l="1a5",m="wr",n="xe",o="230",p="424"};
+Dados={{"x","y",0,0,0,58,",u_h;1g_h;1e_;2sj;1e_;2sj;1c_abeba;1a_abeba;1a_abebga;18_abebga;16_agbebia;14_agbebia;14_agbebka;12_agbebka;10_agb;203bia;10_agb;203bia,k_e,a_agba,4_abgfa,i_e,a_agba,4_abgfa,g_h,4_agb;3kz,vxa,e_h,4_agb;3kz,vxa,e_ag;2sjkbhdlh,4_ag;2sjkbhdlh,4_akb;203obe;1odd;203,4_akb;203obe;1odd;4dfij;1lv;2h8bf;22la,ijd;2sjij;1lv;2h8bf;22la,ijd;36rk;16waobf;22lc;1az;36rk;16waobf;22lc;1azabhpagbf;1odc;1azabhpagbf;1odc;1azagjh;4gcbnlc;1azagjh;4gcbnlc;1azc,2_a;1osbepbfbf,vxcmdc,2_a;1osbepbfbf,vxcmdc,6_ak;16w;2vgcibnbf,hpc,ijdc,8_ak;16w;2vgcibnbf,hpc,ijdc,8_cba;4gccibf,iqnde,a_cba;4gccibf,iqnde,c_cja;4ukbnbfc,i_cja;4ukbnbfc,i_cbgja;3nwbfjfda,g_cbgja;3nwbfjfda,i_aijoaib;1bmde,i_aijoaib;1bmde,m_apbfldcda,k_apbfldcda,o_ab;2h8jflcma,o_ab;2h8jflcma,s_;1lvbgbnldcma,s_;1lvbgbnldcma;10_;3kzdmdc;10_;3kzdmdc;1i_amc;1i_amc;1k_cmc;1k_cmc;1k_c,ijdc;1k_c,ijdc;1m_e;1m_e,6_"}};
+
+-- Dict={a="3de",...};
+-- Dados={
+--   {"x","y",0,0,0,100,"abfasdfasgSKFFAMSOFImisisjdfisaADFAd,fa"},
+--   {}...
+-- };
+
+
+
+local function geraBlocos()end
+
+local function converteBase36PraBlocos()end
+
+local function converteEGera()
+  ;
+end
+
+Simbolos = { ["."]=1,[","]=2,[";"]=3,[":"]=4,["-"]=5,["="]=6,["+"]=7 };
+local function avaliaCaractere(caractere, controle)
+  if controle.base36.lendo then
+    controle.base36.caracteresLidos = controle.base36.caracteresLidos .. caractere;
+    controle.base36.qt = controle.base36.qt - 1;
+    if controle.base36.qt == 0 then
+      controle.base36.lendo = false
+      converteEGera(controle.base36.caracteresLidos, controle);
+    end
+    return;
+  end
+
+  if Simbolos[caractere] ~= nil then
+    controle.base36.lendo = true;
+    controle.base36.qt = Simbolos[caractere];
+    controle.base36.caracteresLidos = "";
+    return;
+  end
+
+  converteEGera( Dict[caractere], controle );
+end
+
+local function geraImagem(origem)
+  local eixo1, eixo2, x, y, z, largura, dadosCompactos = unpack(Dados[1]);
+  local controle = {
+    base36 = {
+      lendo = false,
+      qt = 0,
+      caracteresLidos = ""
+    },
+    larguraImagem = largura,
+    coordenadas = {
+      x = origem.x+x, y = origem.y+y, z = origem.z+z
+    },
+    eixo1 = eixo1, eixo2 = eixo2
+  };
+
+  for i = 1, #dadosCompactos do
+    avaliaCaractere( dadosCompactos:sub(i, i), controle );
+  end
+end
+
+local function clique(evento)
+  local _,ferramenta = Player:getCurToolID(evento.eventobjid);
+  if ferramenta ~= 11569 then --cajado de madeira simples
+    return;
+  end
+
+  Chat:sendSystemMsg("Gerando...");
+  geraImagem({evento.x, evento.y, evento.z});
+end
+
+ScriptSupportEvent:registerEvent('Player.ClickBlock', clique);
