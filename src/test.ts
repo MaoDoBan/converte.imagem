53b69fe9c72476fd0ee2ce5ebb061e045bde2060
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-18 19:32:50 -0300

    test.ts: timming test iteration

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,69 @@
-console.log(Deno.args[0]);
-const [fileName] = Deno.args;
-console.log(fileName.split('.')[0]);
+import { readJsonSync } from "https://deno.land/x/jsonfile@1.0.0/mod.ts";
+
+let ct = [0,0,0];
+function logTempo(passado: number, indice: number, txt: string){
+  let diferença = Date.now() - passado;
+  ct[indice] += diferença;
+  //console.log("Tempo pra "+txt+": "+ ( diferença ) +"ms");
+  return Date.now();
+}
+type Count = { [dado: string]: number };
+
+let passado = Date.now();
+const dadosBrutos = readJsonSync("io/Nanda_dados_brutos.json") as string[];
+//passado = logTempo(passado, "ler arquivo");
+
+let dadu;
+function contar(){
+  const contador1: Count = {};
+  for(const dado of dadosBrutos){
+    if( contador1[dado] ){
+      contador1[dado]++;
+      continue;
+    }
+    contador1[dado] = 1;
+  }
+  passado = logTempo(passado, 0, "contar1");
+  //console.log(contador1);//return;
+
+  const contador2: Count = {};
+  for(const dado of dadosBrutos){
+    dadu = dado;
+    if( !contador2[dadu] ){
+      contador2[dadu] = 1;
+      continue;
+    }
+    contador2[dadu]++;
+  }
+  passado = logTempo(passado, 1, "contar1");
+
+  // const contador2: Count = {};
+  // for(const dado of dadosBrutos){
+  //   contador2[dado] = contador2[dado] ? ++contador2[dado] : 1;
+  // }
+  // passado = logTempo(passado, 1, "contar2   ");
+  // //console.log(contador2);
+
+  // const contador3: Count = {};
+  // for(const dado of dadosBrutos){
+  //   contador3[dado] = 0;
+  // }
+  // for(const dado of dadosBrutos){
+  //   contador3[dado]++;
+  // }
+  // passado = logTempo(passado, 2, "contar3      ");
+  //console.log(contador3);
+}
+//contar();
+for (let i = 0; i < 100000; i++){
+  contar();
+}
+console.log(ct);
+
+
+// console.log(Deno.args[0]);
+// const [fileName] = Deno.args;
+// console.log(fileName.split('.')[0]);
 
 
 // let tabela0 = "0";
@@ -14,22 +77,6 @@ console.log(fileName.split('.')[0]);
 // //console.log(parseInt(tabela, 2))
 
 
-// function começaComOutraEtiqueta(table: string[]){
-//   ;
-// }
-// const tabela = ["0"];
-// ct = 0;
-// let etiqueta = "";
-// for(let i = 1; i < 38; ){
-//   ct++;
-//   etiqueta = ct.toString(2);
-//   if( tabela.includes(etiqueta) ) continue
-//   tabela[i] = etiqueta;
-//   i++;
-// }
-// console.log("tabela:", tabela);
-
-
 // const obi = {fileName: "stringui", lAxis: "x", cAxis: "z", x: 1, y: 11, z: 3};
 // console.log(obi);
 
@@ -37,15 +84,3 @@ console.log(fileName.split('.')[0]);
 
 // const {fileName, ...o2} = obi;
 // console.log(fileName, o2);
-
-
-// for(let iColumn = 0; iColumn < 6; iColumn++){///
-//   //const line: number[] = [];
-//   const ar = [];
-//   for(let iLine = 0; iLine < 3; iLine++){///
-//     const i = iColumn*4 + iLine*4*6;
-//     ar.push(i)//console.log(i);
-//   }
-//   console.log(ar)
-//   //lines.push(line);
-// }
