9ebc57ad214ece40a51df97c6f559a42bfb6b730
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-19 01:55:02 -0300

    refactor Blocks: count and sort ~ordered > soredArray ~count(): properly create Blocks into map and sortedArray, and count -newBlock(): moved to Block class -incrementCount() deprecated +sortByCount(): adapted from std/collections sortBy()

--- src/classes/Convert/Blocks.ts
+++ src/classes/Convert/Blocks.ts
@@ -1,74 +1,43 @@
 import { NumOrString } from "../../interfaces/Types.ts";
-type Block = {
-  base36: string,
-  count: number,
-  next: Block | undefined,
-  previous: Block | undefined
-};
+import { Block } from "./Block.ts";
 type BlocksMap = { [base36: string]: Block };
 
 export class Blocks{
-  private ordered: Block = {} as Block;
-  private endOrdered: Block = {} as Block;
   private map: BlocksMap;
+  private sortedArray: Block[];
 
   constructor(
     blockImages: NumOrString[][]
   ){
     this.map = {};
+    this.sortedArray = [];
     this.count(blockImages);
+    this.sortByCount();
   }
-  get orderedList(): Block{
-    return this.ordered;
+  get sorted(): Block[]{
+    return this.sortedArray;
   }
 
   private count(blockImages: NumOrString[][]){
-    this.ordered = this.newBlock(blockImages[0][6] as string);
-    this.ordered.previous = undefined;
-
     let blockId: string;
     for(const blockImage of blockImages){
-      for(let i = 7; i < blockImage.length; i++){
+      for(let i = 6; i < blockImage.length; i++){
         blockId = blockImage[i] as string;
         if(!this.map[blockId]){
-          this.map[blockId] = this.newBlock(blockId);
+          this.map[blockId] = new Block(blockId);
+          this.sortedArray.push(this.map[blockId]);
           continue;
         }
-        this.countedBlocks[block].ct++;
-        this.countedBlocks[block].size += block.length+1;
+        this.map[blockId].count++;
       }
     }
-    const qtUniques = this.length;
-    return countedBlocks;
   }
 
-  private newBlock(base36: string): Block{
-    const block = {
-      base36,
-      count: 1,
-      next: undefined,
-      previous: this.endOrdered
-    };
-    this.endOrdered = block;
-    return block;
-  }
-
-  private incrementCount(base36: string){
-    let block = this.map[base36];
-    block.count++;
-    if(block.previous && block.previous.count < block.count){
-      
-      block.next = ;
-      block.previous = ;
-    }
+  sortByCount(){
+    this.sortedArray.sort((block1, block2) => {
+      if(block1.count > block2.count) return 1;
+      if(block1.count < block2.count) return -1;
+      return 0;
+    });
   }
 }
-
-// type Block = {
-//   base36: string,
-//   repeat: number,
-//   //mair largura de chave em que ainda vale a pena comprimir
-//   //quanto espaço ocupa sem comprimir
-//   //quanto espaço ocupa comprimindo pra cada comprimento de chave em que vale a pena comprimir
-// };
-//type BlockMap = { [base36: string]: { count: number, size: number } };
