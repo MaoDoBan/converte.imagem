76c35c0232e2ef755bb28d63fb035d8721a80767
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-21 09:07:52 -0300

    ~Block: ~compressedSavedSizes > compressedSavedSizesWithoutDict: matrix to array ~savedSizeWith() > partialSavedSizeWith():   -parameter dictSpace   ~only care about key lenth on this calc

--- src/classes/Convert/Block.ts
+++ src/classes/Convert/Block.ts
@@ -1,14 +1,14 @@
 export class Block{
   private count: number;
   private sizeWithoutCompressing: number;
-  private compressedSavedSizes: number[][];
+  private compressedSavedSizesWithoutDict: number[];
 
   constructor(
     public base36: string
   ){
     this.count = 1;
     this.sizeWithoutCompressing = 0;
-    this.compressedSavedSizes = [];
+    this.compressedSavedSizesWithoutDict = [];
   }
 
   get rawSize(): number{
@@ -21,11 +21,9 @@ export class Block{
     this.count++;
   }
   
-  savedSizeWith(keyLength: number, dictSpace: 3 | 4 | 8): number{
-    if(this.compressedSavedSizes[keyLength])
-    if(dictSpace == 3){//índice da table do lua, pra k=2+
-      ;
-    }
-    return this.count * (this.base36.length - keyLength + 1) - this.base36.length  - keyLength - dictSpace;
+  partialSavedSizeWith(keyLength: number): number{
+    if(this.compressedSavedSizesWithoutDict[keyLength]) return this.compressedSavedSizesWithoutDict[keyLength];
+    this.compressedSavedSizesWithoutDict[keyLength] = this.count * (this.base36.length - keyLength + 1) - this.base36.length  - keyLength;
+    return this.compressedSavedSizesWithoutDict[keyLength];
   }
 }
