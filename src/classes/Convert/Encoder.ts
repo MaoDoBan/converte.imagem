797532ac1e91b93cc7369443b0d39028b79a026a
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-21 09:15:02 -0300

    refactor Encoder: manjor rebuild "from scratch" -all atributes and methods +letters: outside const +blockImages: uncounted raw data +blocks: counted repeted +constructor(): fill blocks from blockImages +get result: return encoded string; actual WIP:   +worthControl: manage search for best encoded result

--- src/classes/Convert/Encoder.ts
+++ src/classes/Convert/Encoder.ts
@@ -1,79 +1,42 @@
 import { NumOrString } from "../../interfaces/Types.ts";
-import { Counter } from "./Counter.ts";
-type BlocksDict = { [block: string]: string };
+import { CountBlocks } from "./CountBlocks.ts";
+import { WorthManager } from "./WorthManager.ts";
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
 
 export class Encoder{
-  private counter: Counter;
-  private blocksDict: BlocksDict;///uma classe só pra isso? com .toString()
-  private dictStr: string;
-  private encoded: string;
+  private blocks: CountBlocks;
 
   constructor(
     private blockImages: NumOrString[][]
   ){
-    this.counter = new Counter(this.blockImages);
-    this.blocksDict = {};
-    this.dictStr = "";
-    this.encoded = "";
+    this.blocks = new CountBlocks(this.blockImages);
   }
 
   get result(): string{
-    return this.run();////this.dict + this.encoded;
-  }
+    const worthControl = new WorthManager( this.blocks.counted );
 
-  private run(): string{
-    const qtUniques = this.counter.countBlocks();
-    this.putHighestCountedBlocksOnDict();
+    // if(countedBlocks.length <= letters.length){
+    //   ;
+    //   return "";///
+    // }
 
-    this.encode();
-    return this.mergeDictAndEncoded();
-    
-    ///se arrayContagem.length menor que 52: retorna montando dict com A-Z
-    //if(qtUniques <= 52) return this.encode();//this.assemble(qtUniques);
+    //;
 
-    ///monta os 46 mais repetidos em a-z A-T, remove eles do array de contagem
-
-    ///calcula a forma mais otimizada...
-  }
-
-  private encode(){
-    const specialChars = " .,;:-=+";
-    let dictEquivalent: string | undefined;
-    let block: string;
-
-    for(const blockImage of this.blockImages){
-      for(let i = 0; i < 6; i++){
-        this.encoded += blockImage[i]+",";
-      }
-      this.encoded += '"';
-      for(let i = 6; i < blockImage.length; i++){
-        block = blockImage[i] as string;
-        dictEquivalent = this.blocksDict[block];
-        if(dictEquivalent){
-          this.encoded += dictEquivalent;
-          continue;
-        }
-        this.encoded += specialChars[block.length]+block;
-      }
-      this.encoded += '"';
-    }
-  }
-  private putHighestCountedBlocksOnDict(){//limite de 53 caracteres a-zA-Z_ no dict
-    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";//.split('');0123456789
-    const limit = this.counter.length < letters.length ? this.counter.length : letters.length;
-    let highest: string;
-
-    for(let i = 0; i < limit; i++){
-      highest = this.counter.popHighest();
-      this.blocksDict[highest] = letters[i];
-      this.dictStr += letters[i]+'="'+highest+'",';
-    }
-    this.dictStr = this.dictStr.slice(0, -1);//removendo o último caractere
-  }
-
-  private mergeDictAndEncoded(): string{
-    //-const script = this.dictStr + this.encoded;
-    return "Dict={" +this.dictStr+ "};\n"+
-           "Dados={{" +this.encoded+ "}};";///pra 0.7 suportar múltiplas imagens
+    return "";///
   }
 }
+
+/*
+    let bLog = "\n";
+    for(const block of counteds){
+      bLog += " "+block.count;
+    }
+    console.log(bLog);
+
+    bLog = "\n";
+    for(const block of this.blocks.sorted){
+      bLog += " "+block.count;
+    }
+    console.log(bLog);
+*/
