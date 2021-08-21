789ef082c86cc3948e35f1b323235f036baadf1c
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-21 15:48:00 -0300

    refactor & feat Block: ~compressedSavedSizesWithoutDict > partialSavedSizes ~partialSavedSizeWith(): calc more accurate and util +toString(): stringuified Block +static arrayToString(): log stringuified Block[]

--- src/classes/Encode/Block.ts
+++ src/classes/Encode/Block.ts
@@ -1,14 +1,14 @@
 export class Block{
   private count: number;
+  private partialSavedSizes: number[];
   private sizeWithoutCompressing: number;
-  private compressedSavedSizesWithoutDict: number[];
 
   constructor(
     public base36: string
   ){
     this.count = 1;
+    this.partialSavedSizes = [];
     this.sizeWithoutCompressing = 0;
-    this.compressedSavedSizesWithoutDict = [];
   }
 
   get rawSize(): number{
@@ -22,8 +22,20 @@ export class Block{
   }
   
   partialSavedSizeWith(keyLength: number): number{
-    if(this.compressedSavedSizesWithoutDict[keyLength]) return this.compressedSavedSizesWithoutDict[keyLength];
-    this.compressedSavedSizesWithoutDict[keyLength] = this.count * (this.base36.length - keyLength + 1) - this.base36.length  - keyLength;
-    return this.compressedSavedSizesWithoutDict[keyLength];
+    if(this.partialSavedSizes[keyLength]) return this.partialSavedSizes[keyLength];
+
+    const dictSizeMin = keyLength == 1 ? 3 : 4 + keyLength;
+    this.partialSavedSizes[keyLength] = this.count * (this.base36.length - keyLength + 1) - this.base36.length - dictSizeMin;
+    return this.partialSavedSizes[keyLength];
+  }
+
+  toString(): string{
+    return `Block: {b36: ${this.base36}, ct: ${this.count}, saveds: [${this.partialSavedSizes.join()}], }`;
+  }
+  static arrayToString(array: Block[]){
+    if(array == undefined) return;
+    for(const block of array){
+      console.log(block.toString());
+    }
   }
 }
