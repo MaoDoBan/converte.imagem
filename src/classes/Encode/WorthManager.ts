49b37ba1d959a2b2c30eb829d05529142f7c25a6
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-21 15:53:03 -0300

    feat WorthManager: +constructor():   +initialize all attributes   +log cataloged +catalogBlocks(): populate cataloged matrix

--- src/classes/Encode/WorthManager.ts
+++ src/classes/Encode/WorthManager.ts
@@ -4,11 +4,37 @@ import { TrackBlocks } from "./TrackBlocks.ts";
 export class WorthManager{
   private best: TrackBlocks;
   private actual: TrackBlocks;
-  private worths: Block[][];
+  private cataloged: Block[][];
+  private baseNotWorth: string[];
 
   constructor( countedBlocks: Block[] ){
-    // this.initialFilterWorth();
-    //tacar tudo que der pra k=1 apenas no compress do best
+    this.baseNotWorth = [];
+    this.cataloged = [[], [], [], [], [], []] as Block[][];
+    this.catalogBlocks(countedBlocks);
+
+    console.log("| cataloged:");
+    for(let i = 1; i < this.cataloged.length; i++){
+      console.log("i =",i,"  qt:",this.cataloged[i].length);
+      Block.arrayToString(this.cataloged[i]);
+    }
+    console.log("| qt baseNotWorth:",this.baseNotWorth.length,"  itens:",this.baseNotWorth);
+
+    this.best = {} as TrackBlocks;//tacar best: só k=1 apenas no compress do best
+    this.actual = {} as TrackBlocks;
+  }
+
+  private catalogBlocks(blocks: Block[]){
+    let economizedSizeMin: number;
+    for(const block of blocks){
+      for(let keyLength = 1; keyLength <= 5; keyLength++){
+        economizedSizeMin = block.partialSavedSizeWith(keyLength);
+        if(economizedSizeMin < 2){
+          if(keyLength == 1) this.baseNotWorth.push(block.base36);
+          break;
+        }
+        this.cataloged[keyLength].push(block);
+      }
+    }
   }
 
   // private sortByWorth(){
@@ -22,15 +48,6 @@ export class WorthManager{
   // }
 }
 
-
-  // filter(array: Block[], sizeOnDic: number, limit: number){
-  //   for(const block of array){
-  //     ;
-  //     e = block.rawSize - rk - k - l - sizeOnDic;
-  //     if(){}
-  //   }
-  // }
-
   // private initialFilterWorth(){
   //   let rawSize: number;
   //   let worths: number[] = [];
@@ -39,9 +56,4 @@ export class WorthManager{
   //     worths[1] = rawSize - block.count - block.base36.length - 9;
   //     if()
   //   }
-  // }
-
-  // get sorted(): Block[]{
-  //   this.sortBy("count");
-  //   return this.array;
   // }
