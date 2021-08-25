84e6f4515d5c04a740f07c1540d62cc2e8d0b9d2
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-25 13:00:11 -0300

    ~Types: change letters and add specialChars

--- src/interfaces/Types.ts
+++ src/interfaces/Types.ts
@@ -3,5 +3,8 @@ export type NumOrString = number | string;
 export type KeyType = 4 | 8;//3 | 
 export type Key = {name: string, type: KeyType};
 export type EncodeDict = { [block: string]: Key };
-export const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 ♫↕‼¶§↑↓→←∟↔▲▼!#$%&()*/<>?@[]^`{|}~ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜø£Ø×ƒáíóúñÑ"
-                      +"ªº¿®¬½¼¡«»░▒▓│┤ÁÂÀ©╣║╗╝¢¥┐└┴┬├─┼ãÃ╚╔╩╦╠═╬¤ðÐÊËÈıÍÎÏ┘┌█¦ÌÓßÔÒõÕµþÞÚÛÙýÝ¯±‗¾¶§÷°¨·¹³²´♥▄▀¸";
+export const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 !#$%&()*/<>?@[]^`{|}~+'";
+//export const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 ♫↕‼¶§↑↓→←∟↔▲▼!#$%&()*/<>?@[]^`{|}~ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜø£Ø×ƒáíóúñÑ"
+//                      +"ªº¿®¬½¼¡«»░▒▓│┤ÁÂÀ©╣║╗╝¢¥┐└┴┬├─┼ãÃ╚╔╩╦╠═╬¤ðÐÊËÈıÍÎÏ┘┌█¦ÌÓßÔÒõÕµþÞÚÛÙýÝ¯±‗¾¶§÷°¨·¹³²´♥▄▀¸";//lua 5.1 não suporta direito isso pra tirar char da string
+
+export const specialChars = " .,;:-=";
