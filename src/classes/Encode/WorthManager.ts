ed604bd93598218ae409aa82474fa0736bceacbb
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-24 23:27:18 -0300

    ~WorthManager: -deprecated sort extra code ideia

--- src/classes/Encode/WorthManager.ts
+++ src/classes/Encode/WorthManager.ts
@@ -46,35 +46,3 @@ export class WorthManager{
     return new TrackWorthKeys(this.cataloged).result;
   }
 }
-
-/*
-  private sortBySavedSize(){//bigger first
-    let lastBlock: Block;
-    let lastSavedSize: number;
-    let catalogedArray: Block[];
-    let length: number;
-    for(let i = 1; i < this.cataloged.length; i++){
-      catalogedArray = this.cataloged[i];
-      catalogedArray.sort((block1, block2) => {
-        const [saved1, saved2] = [block1.partialSavedSizeWith(i), block2.partialSavedSizeWith(i)];
-        if(saved1 < saved2) return 1;
-        if(saved1 > saved2) return -1;
-        return 0;
-      });
-
-      length = catalogedArray.length;
-      if( catalogedArray.length < 2 || //se tamanho é menor que 2
-          catalogedArray[length-1].partialSavedSizeWith(i) <= catalogedArray[length-2].partialSavedSizeWith(i) )//se o último item é menor ou igual o penúltimo
-        continue;
-      
-      lastBlock = catalogedArray.pop() as Block;
-      lastSavedSize = lastBlock.partialSavedSizeWith(i);
-      for(let j = length; j >= 0; j--){ //ordenar direito o último item
-        if(lastSavedSize <= catalogedArray[j].partialSavedSizeWith(i)){
-          catalogedArray.splice(j+1, 0, lastBlock);
-          break;
-        }
-      }
-    }
-  }
-*/
