abb9d0b2809a5d55eb5c3c0cbc59be421ca63a24
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-25 13:07:13 -0300

    ~Encoder: last needed features to 0.7.0 -blocks +keys ~constructor(): initialize keys witch result from WorthManager,   using counted blocks from CountBlocks ~get result(): return concat returns from encodeDict() & encodeBlocks() ~encodeDict(): to string keys and blocks ~encodeBlocks(): compress blocks witch keys

--- src/classes/Encode/Encoder.ts
+++ src/classes/Encode/Encoder.ts
@@ -1,24 +1,55 @@
-import { NumOrString } from "../../interfaces/Types.ts";
+import { EncodeDict, NumOrString, specialChars } from "../../interfaces/Types.ts";
 import { CountBlocks } from "./CountBlocks.ts";
 import { WorthManager } from "./WorthManager.ts";
 
 export class Encoder{
-  private blocks: CountBlocks;
+  private keys: EncodeDict;
 
   constructor(
     private blockImages: NumOrString[][]
   ){
-    this.blocks = new CountBlocks(this.blockImages);
+    const countedBlocks = new CountBlocks(this.blockImages).counted;
+    console.log("| qt countedBlocks:", countedBlocks.length);
+
+    this.keys = new WorthManager( countedBlocks ).result;
+    console.log(">> this.keys:", this.keys);
   }
 
   get result(): string{
-    const contedBlocks = this.blocks.counted;
-    console.log("| qt contedBlocks:", contedBlocks.length);
-    //-console.log("contedBlocks:", contedBlocks);
+    return `Dict={${this.encodeDict()}};\n`+
+         `Dados={{${this.encodeBlocks()}}};`;////add suporte a múltiplas imagens?
+  }
 
-    const encodeMap = new WorthManager( contedBlocks ).result;
-    console.log(">> encodeMap:", encodeMap);
+  private encodeDict(): string{
+    let encoded = "";
+    for(const block in this.keys){
+      if(this.keys[block].type == 4){
+        encoded += `${this.keys[block].name}="${block}",`;
+        continue;
+      }
+      encoded += `["${this.keys[block].name}"]="${block}",`;
+    }
+    return encoded.slice(0, -1);//removendo o último caractere
+  }
 
-    return "";
+  private encodeBlocks(): string{
+    let encoded = "";
+    let block: string;
+    for(const blockImage of this.blockImages){
+      for(let i = 0; i < 6; i++){
+        encoded += blockImage[i]+",";
+      }
+      encoded += '"';
+      for(let i = 6; i < blockImage.length; i++){
+        block = blockImage[i] as string;
+        if(this.keys[block]){
+          encoded += this.keys[block].name;
+          continue;
+        }
+        encoded += specialChars[block.length] + block;
+      }
+      encoded += '"';
+    }
+    return encoded;
   }
 }
