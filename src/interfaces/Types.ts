5dae8a60fe5a67164292973e6b2c3896129d0cdd
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-24 21:57:12 -0300

    ~Types: +type KeyType +type Key +type EncodeDict +const letters: all chars to use in keys

--- src/interfaces/Types.ts
+++ src/interfaces/Types.ts
@@ -1,3 +1,7 @@
 export type NumOrString = number | string;
 
-//export type CountMap = { [block: string]: { ct: number, size: number } };
+export type KeyType = 4 | 8;//3 | 
+export type Key = {name: string, type: KeyType};
+export type EncodeDict = { [block: string]: Key };
+export const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 ♫↕‼¶§↑↓→←∟↔▲▼!#$%&()*/<>?@[]^`{|}~ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜø£Ø×ƒáíóúñÑ"
+                      +"ªº¿®¬½¼¡«»░▒▓│┤ÁÂÀ©╣║╗╝¢¥┐└┴┬├─┼ãÃ╚╔╩╦╠═╬¤ðÐÊËÈıÍÎÏ┘┌█¦ÌÓßÔÒõÕµþÞÚÛÙýÝ¯±‗¾¶§÷°¨·¹³²´♥▄▀¸";
